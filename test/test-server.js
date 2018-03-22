f5effa2e9ee823109a035d23000e64bf49484dd6
Stephanie <stephaniejoychamblee@gmail.com>
2018-03-22 13:41:52 -0400

    Replace createAccount with register in test

--- test/test-server.js
+++ test/test-server.js
@@ -28,10 +28,10 @@ describe('feedback page', function () {
   });
 });
 
-describe('create account page', function () {
+describe('register page', function () {
   it('should exist', function () {
     return chai.request(app)
-      .get('/createAccount')
+      .get('/register')
       .then(function (res) {
         expect(res).to.have.status(200);
       });
